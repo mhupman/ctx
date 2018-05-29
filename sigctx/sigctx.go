2171df1b1487914a75bf6b332b84f46fb8476e4d
mhupman <mhupman@yext.com>
2018-05-29 09:45:26 -0400

    sigctx: lazy-load initialization using sync.Once in the New() method; call signal.Stop() after processing is complete
    This change prevents a situation where a program that imports this library but doesn't use (code calling sigctx.New() isn't invoked) no longer exits in response to SIGINT.

--- sigctx/sigctx.go
+++ sigctx/sigctx.go
@@ -4,29 +4,34 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 )
 
-var ctx context.Context
-
-func init() {
-	var cancel context.CancelFunc
-	ctx, cancel = context.WithCancel(context.Background())
-
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		select {
-		case <-ch:
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
-}
+var (
+	ctx  context.Context
+	once sync.Once
+)
 
 // New signal-bound context.  Context terminates when either SIGINT or SIGTERM
 // is caught.
 func New() context.Context {
+	once.Do(func() {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithCancel(context.Background())
+
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+
+		go func() {
+			select {
+			case <-ch:
+				cancel()
+			case <-ctx.Done():
+			}
+			signal.Stop(ch)
+		}()
+	})
+
 	return ctx
 }
